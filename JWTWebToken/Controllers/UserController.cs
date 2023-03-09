ec9db6dd7965c064efefe0309396a814eebd9601
sudeer48 <sudeer0408@gmail.com>
2023-03-09 12:00:15 +0530

    Initial commit
    Initial commit

---
+++ JWTWebToken/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using JWTWebToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Claims;
+
+namespace JWTWebToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //For admin Only
+        [HttpGet]
+        [Route("Admins")]
+        [Authorize]
+        public IActionResult AdminEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi you are an {currentUser.Role}");
+        }
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+
+    }
+}
---
+++ JWTWebToken/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿namespace JWTWebToken.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="naeem",Password="naeem_admin",Role="Admin1"}
+            };
+    }
+}
